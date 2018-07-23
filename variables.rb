e85453867e4c10a80db8d2ba1f59ea81ca67309d
emilyambler <emilyambler4@gmail.com>
2018-07-23 19:02:44 +0000

    automatically backed up by learn

--- variables.rb
+++ variables.rb
@@ -4,8 +4,8 @@
 # creates comment that the computer won't run as code 
 # highlight and press control or command / to turn code into comments
 
-name = "emily"
-puts name
+# name = "emily"
+# puts name
 age = 17
 puts "my name is #{name}, and I am #{age} years old"
 # {}
